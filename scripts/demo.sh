e3f4d98a240031e70bb39bea29f54377dabb6b0d
harshana20011 <runh1501.usherbrooke.ca>
2024-09-07 21:23:58 -0400

    deleted --multiprocess argument in parser

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -10,7 +10,7 @@ python build_random_adj_matrix.py 5 6 True rand_graph
 
 python draw_random_adj_matrix.py rand_graph rand_graph_visu
 
-python find_max_intensity_diffusion_path.py rand_graph 1 0 qaoa_solver_infos --alphas 1 2 3 --reps 1 --multiprocess -npr 2
+python find_max_intensity_diffusion_path.py rand_graph 1 0 qaoa_solver_infos --alphas 1 2 3 --reps 1  -npr 2
 
 python plot_distribution_probabilities.py qaoa_solver_infos_alpha_1.npz qaoa_solver_infos_alpha_2.npz qaoa_solver_infos_alpha_3.npz visu_total_dist visu_selected_dist
 
