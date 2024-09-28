8864f8a5c9a859f3f0d86e97d4f42f256b647672
harshana20011 <runh1501.usherbrooke.ca>
2024-09-28 16:23:29 +0200

    visualisation, verification and dist plot good result to change, as well as random number (maybe int would be easier to verify to start)

--- quactography/hamiltonian/hamiltonian_qubit_node.py
+++ quactography/hamiltonian/hamiltonian_qubit_node.py
@@ -26,6 +26,7 @@ class Hamiltonian_qubit_node:
                 (self.starting_node_c) ** 2 + (self.ending_node_c) ** 2 + self.hint_c
             ).simplify()
         )
+        self.exact_cost, self.exact_path = self.get_exact_sol()
 
     def mandatory_cost(self):
         """Cost of going through a path
@@ -283,6 +284,21 @@ class Hamiltonian_qubit_node:
         sum_intermediate_cost_h_terms = initial_int_term_h
 
         return sum_intermediate_cost_h_terms
+
+    def get_exact_sol(self):
+        mat_hamiltonian = np.array(self.total_hamiltonian.to_matrix())
+        eigenvalues, eigenvectors = np.linalg.eig(mat_hamiltonian)
+
+        best_indices = np.where(eigenvalues == np.min(eigenvalues))
+        # print(eigenvalues[int("0111", 2)])
+        # print("Eigenvalues : ", eigenvalues[best_indices])
+        # print("Eigenvectors : ", eigenvectors[best_indices])
+
+        binary_paths = [bin(idx[0]).lstrip("-0b") for idx in best_indices]
+        # print("Binary paths : ", binary_paths)
+
+        # costs and paths to all best solutions
+        return eigenvalues[best_indices], binary_paths
 
 
 # # Test the Hamiltonian_qubit_node class:
