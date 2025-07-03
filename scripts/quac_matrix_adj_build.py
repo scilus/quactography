2a6c8001a1f3583d06dadc6bab50d64d7413e30a
Desk3505 <deemunit@gmail.com>
2025-07-03 15:07:42 -0400

    command script to function

--- scripts/quac_matrix_adj_build.py
+++ scripts/quac_matrix_adj_build.py
@@ -24,74 +24,75 @@ Tool to build adjacency matrix from diffusion data (white matter mask and fodf p
 """
 
 
-def _build_arg_parser():
-    p = argparse.ArgumentParser(
-        description=__doc__, formatter_class=argparse.RawTextHelpFormatter
-    )
-    p.add_argument("in_nodes_mask",
-                   help="Input nodes mask image (.nii.gz file)")
-    p.add_argument("in_sh",
-                   help="Input SH image. (nii.gz file)")
-    p.add_argument("out_graph",
-                   help="Output graph file name (npz file)")
+# def _build_arg_parser():
+#     p = argparse.ArgumentParser(
+#         description=__doc__, formatter_class=argparse.RawTextHelpFormatter
+#     )
+#     p.add_argument("in_nodes_mask",
+#                    help="Input nodes mask image (.nii.gz file)")
+#     p.add_argument("in_sh",
+#                    help="Input SH image. (nii.gz file)")
+#     p.add_argument("out_graph",
+#                    help="Output graph file name (npz file)")
 
-    p.add_argument(
-        "--keep_mask",
-        help="Nodes that must not be filtered out."
-        )
-    p.add_argument(
-        "--threshold",
-        default=0.0,
-        type=float,
-        help="Cut all weights below a given threshold. [%(default)s]",
-    )
-    p.add_argument(
-        "--slice_index",
-        type=int,
-        help="If None, midslice is taken."
-        )
-    p.add_argument(
-        "--axis_name",
-        default="axial",
-        choices=["sagittal", "coronal", "axial"],
-        help="Axis along which a slice is taken.",
-    )
-    p.add_argument(
-        "--save_only",
-        action="store_true",
-        help="Does not plot the matrix, only saves a copy of npz adjacency matrix file"
-    )
-    return p
+#     p.add_argument(
+#         "--keep_mask",
+#         help="Nodes that must not be filtered out."
+#         )
+#     p.add_argument(
+#         "--threshold",
+#         default=0.0,
+#         type=float,
+#         help="Cut all weights below a given threshold. [%(default)s]",
+#     )
+#     p.add_argument(
+#         "--slice_index",
+#         type=int,
+#         help="If None, midslice is taken."
+#         )
+#     p.add_argument(
+#         "--axis_name",
+#         default="axial",
+#         choices=["sagittal", "coronal", "axial"],
+#         help="Axis along which a slice is taken.",
+#     )
+#     p.add_argument(
+#         "--save_only",
+#         action="store_true",
+#         help="Does not plot the matrix, only saves a copy of npz adjacency matrix file"
+#     )
+#     return p
 
 
-def main():
+def build_mat(in_nodes_mask, in_sh, out_graph,
+         keep_mask=None, threshold=0.2, slice_index=None,
+         axis_name="axial", save_only=False):
     
-    parser = _build_arg_parser()
-    args = parser.parse_args()
-    nodes_mask_im = nib.load(args.in_nodes_mask)
-    sh_im = nib.load(args.in_sh)
+    
+    nodes_mask_im = nib.load(in_nodes_mask)
+    sh_im = nib.load(in_sh)
 
     nodes_mask = slice_along_axis(
-        nodes_mask_im.get_fdata().astype(bool), args.axis_name, args.slice_index
+        nodes_mask_im.get_fdata().astype(bool), axis_name, slice_index
     )
 
     keep_node_indices = None
-    if args.keep_mask:
+    if keep_mask:
         keep_mask_im = nib.load(keep_mask)
         keep_mask = slice_along_axis(
-            keep_mask_im.get_fdata().astype(bool), args.axis_name, args.slice_index
+            keep_mask_im.get_fdata().astype(bool), axis_name, slice_index
         )
         keep_node_indices = np.flatnonzero(keep_mask)
 
     # !!Careful, we remove a dimension, but the SH amplitudes still exist in 3D
-    sh = slice_along_axis(sh_im.get_fdata(), args.axis_name, args.slice_index)
+    sh = slice_along_axis(sh_im.get_fdata(), axis_name, slice_index)
 
     # adjacency graph
     adj_matrix, node_indices = build_adjacency_matrix(nodes_mask)
 
     # assign edge weights
     weighted_graph, node_indices = build_weighted_graph(
-        adj_matrix, node_indices, sh, args.axis_name
+        adj_matrix, node_indices, sh, axis_name
     )
 
     # # Could be added in the code if needed:
@@ -104,7 +105,7 @@ def main():
     # # _______________________________________________________________________
 
     # filter graph edges by weight
-    weighted_graph[weighted_graph < args.threshold] = 0.0
+    weighted_graph[weighted_graph < threshold] = 0.0
 
     # remove intermediate nodes that connect only two nodes
     weighted_graph = remove_intermediate_connections(
@@ -116,15 +117,15 @@ def main():
         weighted_graph, node_indices, keep_node_indices
     )
 
-    if not args.save_only:
+    if not save_only:
         plt.imshow(np.log(weighted_graph + 1))
         plt.show()
 
     # print("node indices", node_indices)
     # save output
-    save_graph(weighted_graph, node_indices, nodes_mask.shape, args.out_graph)
+    save_graph(weighted_graph, node_indices, nodes_mask.shape, out_graph)
     print("Graph saved")
-    path = Path(args.out_graph)
+    path = Path(out_graph)
 
     rap_funct(
         path,
@@ -137,5 +138,7 @@ def main():
     
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     parser = _build_arg_parser()
+#     args = parser.parse_args()
+#     main(args.in_nodes_mask, args.in_sh, args.out_graph)
